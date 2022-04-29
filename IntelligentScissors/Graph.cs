9df16a803d9cff8d6924e47609b38c3beae74ee0
EslamEsam <62443536+EslamEsam@users.noreply.github.com>
2022-04-29 19:30:33 +0200

    Add project files.

---
+++ IntelligentScissors/Graph.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelligentScissors
+{
+    
+    public struct edge_weight
+    {
+        public double weight;
+        public string edge;
+    }
+    
+    internal class Graph
+    {
+        public Dictionary<string, List<edge_weight>> graph = new Dictionary<string, List<edge_weight>>();
+        List<edge_weight> edges = new List<edge_weight>();
+        public void Add_vertices(RGBPixel[,] ImageMatrix)
+        {
+            for (int i = 0; i < ImageMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < ImageMatrix.GetLength(1); j++)
+                {
+                    string key = i.ToString() + "," + j.ToString();
+                    graph.Add(key, new List<edge_weight>());
+                }
+            }
+        }
+
+        public void Add_edges (RGBPixel[,] ImageMatrix)
+        {
+            for (int i = 0; i < ImageMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < ImageMatrix.GetLength(1); j++)
+                {
+                    string key = i.ToString() + "," + j.ToString();
+                    if (i == 0) 
+                    {
+                        if (j == 0)
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i + 1).ToString() + "," + j.ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j + 1).ToString() });
+
+                        }
+                        else if (j == (ImageMatrix.GetLength(1)-1))
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j - 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i + 1).ToString() + "," + j.ToString() });
+                        }
+                        else
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i + 1).ToString() + "," + j.ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j + 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j - 1).ToString() });
+                        }
+
+                    }
+
+                    else if (i == (ImageMatrix.GetLength(0)-1))
+                    {
+                        if (j == 0)
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i-1).ToString() + "," + j.ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j + 1).ToString() });
+
+                        }
+                        else if (j == (ImageMatrix.GetLength(1)-1))
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j - 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i - 1).ToString() + "," + j.ToString() });
+                        }
+                        else
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j + 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j - 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i - 1).ToString() + "," + j.ToString() });
+                        }
+                    }
+                    else
+                    {
+                        if (j == 0)
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i-1).ToString() + "," + (j).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j + 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i + 1).ToString() + "," + j.ToString() });
+                        }
+                        else if (j == (ImageMatrix.GetLength(1)-1))
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j + 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i + 1).ToString() + "," + (j).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i - 1).ToString() + "," + j.ToString() });
+                        }
+                        else
+                        {
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i + 1).ToString() + "," + j.ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j + 1).ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i - 1).ToString() + "," + j.ToString() });
+                            graph[key].Add(new edge_weight { weight = 0, edge = (i).ToString() + "," + (j - 1).ToString() });
+                        }
+                    }
+                    
+                }
+            }
+            
+        }
+
+
+    }
+}
---
+++ IntelligentScissors/MainForm.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IntelligentScissors
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        RGBPixel[,] ImageMatrix;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Open the browsed image and display it
+                string OpenedFilePath = openFileDialog1.FileName;
+                ImageMatrix = ImageOperations.OpenImage(OpenedFilePath);
+                Graph graph = new Graph();
+                graph.Add_vertices(ImageMatrix);
+                graph.Add_edges(ImageMatrix);
+                for (int i = 0; i < graph.graph.Count; i++)
+                {
+                    for (int j = 0; j < graph.graph.Values.Count; j++)
+                    {
+                        string key = i.ToString() + "," + j.ToString();
+                        if (graph.graph.ContainsKey(key))
+                        {
+                            foreach (var item in graph.graph[key])
+                            {
+
+                                Console.WriteLine("Vertex : " + key + "Edge : " + item.edge);
+
+                            }
+                        }
+                    }
+                }
+                ImageOperations.DisplayImage(ImageMatrix, pictureBox1);
+            }
+            txtWidth.Text = ImageOperations.GetWidth(ImageMatrix).ToString();
+            txtHeight.Text = ImageOperations.GetHeight(ImageMatrix).ToString();
+        }
+
+        private void btnGaussSmooth_Click(object sender, EventArgs e)
+        {
+            double sigma = double.Parse(txtGaussSigma.Text);
+            int maskSize = (int)nudMaskSize.Value ;
+            ImageMatrix = ImageOperations.GaussianFilter1D(ImageMatrix, maskSize, sigma);
+            ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
+        }
+
+       
+       
+    }
+}
